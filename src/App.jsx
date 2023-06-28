b5c62176cb6e7008880d2430b7043c3fab7be1fb
ToanKitsu <nguyenngoctoanrc@gmail.com>
2023-06-28 22:00:20 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Galery from './Galery';
+import SearchForm from './SearchForm';
+import ThemeToggle from './ThemeToggle';
+
+const App = () => {
+  return (
+    <main>
+      <ThemeToggle />
+      <SearchForm />
+      <Galery />
+    </main>
+  );
+};
+export default App;
